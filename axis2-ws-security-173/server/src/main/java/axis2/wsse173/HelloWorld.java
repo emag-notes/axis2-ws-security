6c7992cd44b121c4c208e2d6e74787940b9f911b
emag <lanabe.lanabe@gmail.com>
2016-07-22 14:20:09 +0900

    Change to without JAX-WS Annotation and use rampart config

--- axis2-ws-security-173/server/src/main/java/axis2/wsse173/HelloWorld.java
+++ axis2-ws-security-173/server/src/main/java/axis2/wsse173/HelloWorld.java
@@ -1,14 +1,8 @@
 package axis2.wsse173;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebService;
-
-@WebService(serviceName = "HelloWorld")
 public class HelloWorld {
 
-  @WebMethod
-  public String sayHello(@WebParam(name = "text") String text) {
+  public String sayHello(String text) {
     return "Hello, " + text;
   }
 
